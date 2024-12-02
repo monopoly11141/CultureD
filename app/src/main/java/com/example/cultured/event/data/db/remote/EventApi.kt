584e1c76709f263f54f907b1009dcd6078946153
monopoly11141 <monopoly1114@vt.edu>
2024-12-02 13:58:52 +0900

    Working: Getting Data from api

--- app/src/main/java/com/example/cultured/event/data/db/remote/EventApi.kt
+++ app/src/main/java/com/example/cultured/event/data/db/remote/EventApi.kt
@@ -1,4 +1,11 @@
 package com.example.cultured.event.data.db.remote
 
+import com.example.cultured.event.data.model.EventModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface EventApi {
+    @GET("1/1000/%20/%20")
+    fun boardListPost(@Query("date") date: String): Call<EventModel>
 }
---
+++ app/src/main/java/com/example/cultured/event/data/model/EventModel.kt
@@ -0,0 +1,47 @@
+package com.example.cultured.event.data.model
+
+import com.tickaroo.tikxml.annotation.Element
+import com.tickaroo.tikxml.annotation.PropertyElement
+import com.tickaroo.tikxml.annotation.Xml
+
+@Xml(name = "culturalEventInfo")
+data class EventModel(
+    @PropertyElement(name = "list_total_count")
+    val totalCount: Long = 0,
+    @Element(name = "row")
+    val eventList: List<Event> = emptyList()
+)
+
+@Xml(name = "row")
+data class Event(
+    @PropertyElement(name = "CODENAME")
+    val codeName: String = "",
+    @PropertyElement(name = "GUNAME")
+    val guName: String = "",
+    @PropertyElement(name = "DATE")
+    val date : String = "",
+    @PropertyElement(name = "PLACE")
+    val place: String = "",
+    @PropertyElement(name = "ORG_NAME")
+    val orgName: String = "",
+    @PropertyElement(name = "USE_TRGT")
+    val useTarget: String  ="",
+    @PropertyElement(name = "USE_FEE")
+    val useFee : String  = "",
+    @PropertyElement(name = "ORG_LINK")
+    val organizationLink : String  = "",
+    @PropertyElement(name = "MAIN_IMG")
+    val mainImage: String = "",
+    @PropertyElement(name = "STRTDATE")
+    val startDate: String = "",
+    @PropertyElement(name = "END_DATE")
+    val endDate: String = "",
+    @PropertyElement(name = "THEMECODE")
+    val themeCode: String = "",
+    @PropertyElement(name = "LOT")
+    val longitude: String = "",
+    @PropertyElement(name = "LAT")
+    val latitude: String  = "",
+    @PropertyElement(name = "IS_FREE")
+    val isFree: String = ""
+)
